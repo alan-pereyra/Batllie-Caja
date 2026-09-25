a600bfcb64619308287db4ea3a5a0c341f770a39
alan-pereyra <alanpereyra57@gmail.com>
2026-09-25 14:47:11 -0300

    feat: direcciones de envio primero arriba y facturacion abajo al 100% de ancho en movil (v1.2.8)

--- templates/woocommerce/checkout/thankyou.php
+++ templates/woocommerce/checkout/thankyou.php
@@ -37,6 +37,76 @@ defined('ABSPATH') || exit;
 			Batllie_Caja_Tracking::render_tracking_widget($order->get_id());
 			?>
 
+			<style>
+			@media (max-width: 768px) {
+				.woocommerce-customer-details,
+				.woocommerce-order-details + .woocommerce-customer-details {
+					width: 100% !important;
+					margin-top: 30px !important;
+					padding: 0 !important;
+					box-sizing: border-box !important;
+					clear: both !important;
+				}
+				.woocommerce-customer-details .woocommerce-columns--addresses,
+				.woocommerce-customer-details .woocommerce-columns--2,
+				.woocommerce-customer-details .woocommerce-columns,
+				.woocommerce-customer-details .col2-set,
+				.woocommerce-columns--addresses.col2-set,
+				.woocommerce-columns.col2-set {
+					display: flex !important;
+					flex-direction: column !important;
+					width: 100% !important;
+					max-width: 100% !important;
+					float: none !important;
+					clear: both !important;
+					gap: 24px !important;
+					margin-left: 0 !important;
+					margin-right: 0 !important;
+					box-sizing: border-box !important;
+				}
+				.woocommerce-customer-details .woocommerce-column--shipping-address,
+				.woocommerce-customer-details .col-2.woocommerce-column--shipping-address,
+				.woocommerce-customer-details .col-2,
+				.woocommerce-columns--addresses .woocommerce-column--shipping-address,
+				.woocommerce-columns--addresses .col-2 {
+					order: 1 !important;
+					width: 100% !important;
+					max-width: 100% !important;
+					min-width: 100% !important;
+					float: none !important;
+					margin: 0 !important;
+					box-sizing: border-box !important;
+				}
+				.woocommerce-customer-details .woocommerce-column--billing-address,
+				.woocommerce-customer-details .col-1.woocommerce-column--billing-address,
+				.woocommerce-customer-details .col-1,
+				.woocommerce-columns--addresses .woocommerce-column--billing-address,
+				.woocommerce-columns--addresses .col-1 {
+					order: 2 !important;
+					width: 100% !important;
+					max-width: 100% !important;
+					min-width: 100% !important;
+					float: none !important;
+					margin: 0 !important;
+					box-sizing: border-box !important;
+				}
+				.woocommerce-customer-details .woocommerce-column,
+				.woocommerce-customer-details address {
+					width: 100% !important;
+					max-width: 100% !important;
+					min-width: 100% !important;
+					box-sizing: border-box !important;
+					display: block !important;
+				}
+				.woocommerce-customer-details .woocommerce-column__title {
+					width: 100% !important;
+					margin-bottom: 12px !important;
+					font-size: 1.35rem !important;
+					white-space: normal !important;
+				}
+			}
+			</style>
+
 		<?php endif; ?>
 
 		<?php do_action('woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id()); ?>
